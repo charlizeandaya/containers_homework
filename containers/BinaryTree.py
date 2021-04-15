4646ace7f9767eee4b3ffacbf06a086191f6224a
Charlize Andaya <charlize@Charlizes-MacBook.local>
2021-04-15 16:05:57 -0700

    fix flake8

--- containers/BinaryTree.py
+++ containers/BinaryTree.py
@@ -77,8 +77,7 @@ class BinaryTree():
         elif traversal_type == 'postorder':
             return self.postorder_print(self.root, '')
         else:
-            raise ValueError('Traversal type ' +
-                             str(traversal_type) + ' is not supported.')
+            raise ValueError(f'Traversal type {traversal_type} not supported.')
 
     def preorder_print(self, start, traversal):
         '''
@@ -139,8 +138,7 @@ class BinaryTree():
         elif traversal_type == 'postorder':
             return self.postorder(self.root, num_list)
         else:
-            raise ValueError('Traversal type ' +
-                             str(traversal_type) + ' is not supported.')
+            raise ValueError(f'Traversal type {traversal_type} not supported.')
 
     def preorder(self, start, traversal=[]):
         '''
