6f05ed7e3e8d5acb6446b38b1703fab5c0d5f14f
Charlize Andaya <charlize@Charlizes-MacBook.local>
2021-04-20 17:12:37 -0700

    update avltree

--- containers/BST.py
+++ containers/BST.py
@@ -4,8 +4,7 @@ The functions in this file are considerably harder than the functions
 in the BinaryTree file.
 '''
 
-from BinaryTree import BinaryTree, Node
-# from BinaryTree import BinaryTree, Node
+from containers.BinaryTree import BinaryTree, Node
 
 
 class BST(BinaryTree):
